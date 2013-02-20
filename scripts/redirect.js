3f28cd4a44f19156d6341a11ebb919ab7e299ae6
jack gurulian <jack.gurulian@gmail.com>
2013-02-20 19:25:35 +0200

    added rickroll script

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,7 +1,7 @@
 // Script that will redirect victim to a new page when the first page
 // loads
 
-<script language=javascript>
+<script type="text/javascript">
 function redirect(){
   window.location = "http://example.com";
 }
