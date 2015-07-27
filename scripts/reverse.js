c87536b9171dcdd2a96def9fb608518dee18e8c4
Robbie <txt3rob@gmail.com>
2015-07-27 15:24:26 +0100

    Update reverse.js

--- scripts/reverse.js
+++ scripts/reverse.js
@@ -1,7 +1,7 @@
 // Reverses images
 
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<script type="text/javascript" src="https://jqueryrotate.googlecode.com/files/jQueryRotate.2.2.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://raw.githubusercontent.com/wilq32/jqueryrotate/master/jQueryRotate.js"></script>
 <script type="text/javascript">
 
 window.onload=function() {
