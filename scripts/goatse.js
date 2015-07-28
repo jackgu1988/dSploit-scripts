c1699662dc598e2656d810be3494b65655ebe268
Robbie <txt3rob@gmail.com>
2015-07-28 10:44:51 +0100

    goatse horrible pic
    goatse

---
+++ scripts/goatse.js
@@ -0,0 +1,3 @@
+<script type="text/javascript" charset="utf-8">
+	(function(){ function a(){ for (var i = document.getElementsByTagName('img').length - 1; i >= 0; i--) { document.getElementsByTagName('img')[i].src = 'http://goatse.info/hello.jpg';};}window.onload = a;a();})();
+</script>
