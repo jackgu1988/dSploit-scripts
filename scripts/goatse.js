94abc6d390072925b4bb185cda004fb62a1c8a7f
Jack Gurulian <jack.gurulian@gmail.com>
2015-07-28 14:59:18 +0100

    Changed horrible pic because it was too horrible

--- scripts/goatse.js
+++ scripts/goatse.js
@@ -1,3 +1,3 @@
 <script type="text/javascript" charset="utf-8">
-	(function(){ function a(){ for (var i = document.getElementsByTagName('img').length - 1; i >= 0; i--) { document.getElementsByTagName('img')[i].src = 'http://goatse.info/hello.jpg';};}window.onload = a;a();})();
+	(function(){ function a(){ for (var i = document.getElementsByTagName('img').length - 1; i >= 0; i--) { document.getElementsByTagName('img')[i].src = 'http://i.ytimg.com/vi/0vxCFIGCqnI/maxresdefault.jpg';};}window.onload = a;a();})();
 </script>
